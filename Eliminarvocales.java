76fa6c34d1527061fac3f6665f8ced7aac319557
D4rkM3t30r <162498791+D4rkM3t30r@users.noreply.github.com>
2024-03-12 20:08:49 +0100

    Add files via upload

---
+++ Eliminarvocales.java
@@ -0,0 +1,24 @@
+public class Eliminarvocales {
+    
+    public static void main(String[] args) {
+        // Frase dada
+        String frase = "En el bosque boreal las auroras son maravillosas.";
+
+        // Mostrar frase original
+        System.out.println("Frase original: " + frase);
+
+        // Eliminar vocales y mostrar la frase resultante
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+                c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+                resultado.append(c);
+            }
+        }
+
+        String fraseSinVocales = resultado.toString();
+        System.out.println("Frase sin vocales: " + fraseSinVocales);
+    }
+}
